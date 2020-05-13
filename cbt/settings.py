91bbdd2379448fdaad3c42468197459ceba1cda3
Anefu Favour <favouranefu@gmail.com>
2020-05-13 14:59:52 +0100

    Last required config

--- cbt/settings.py
+++ cbt/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_ev&692n8rsy-whra_&^)n3hzs*^djzm+pi89ydn)gt^y2eo2l'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '_ev&692n8rsy-whra_&^)n3hzs*^djzm+pi89ydn)gt^y2eo2l')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
